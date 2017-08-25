a3b0bf7b54656c3f0736d52fb7c0941ea8010cbe
Matthias Kuhn <matthias@opengis.ch>
2017-08-25 09:58:34 +0200

    Fix github deploy key

--- scripts/ci/setup_git.sh
+++ scripts/ci/setup_git.sh
@@ -2,7 +2,7 @@
 
 if test "$TRAVIS_SECURE_ENV_VARS" = "true" -a "$TRAVIS_BRANCH" = "master";
 then
-  openssl aes-256-cbc -K $encrypted_e87d7d0ced73_key -iv $encrypted_e87d7d0ced73_iv -in opengisch_rsa.enc -out opengisch_rsa -d;
+  openssl aes-256-cbc -K $encrypted_e87d7d0ced73_key -iv $encrypted_e87d7d0ced73_iv -in opengisch_rsa.enc -out ~/.ssh/id_rsa -d;
   chmod 600 ~/.ssh/id_rsa;
   git config --global user.email "info@opengis.ch";
   git config --global user.name "opengisch";
