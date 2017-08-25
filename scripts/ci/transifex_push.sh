7020dd649f0f0af22149fc95ea76764616f147e5
Matthias Kuhn <matthias@opengis.ch>
2017-08-25 15:40:35 +0200

    Only pull translations on master builds

--- scripts/ci/transifex_push.sh
+++ scripts/ci/transifex_push.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-if test "$TRAVIS_SECURE_ENV_VARS" = "true" -a "$TRAVIS_BRANCH" = "master";
+if test "$TRAVIS_SECURE_ENV_VARS" = "true" -a "$TRAVIS_BRANCH" = "master" -a "$TRAVIS_PULL_REQUEST" = "false";
 then
   echo -e "[https://www.transifex.com]\nhostname = https://www.transifex.com\nusername = api\npassword = ${TRANSIFEX_API_TOKEN}\ntoken =\n" > ~/.transifexrc
   make gettext
