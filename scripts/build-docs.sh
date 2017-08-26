2ea4f9ae20d576d5858b4b6fe92d4a170a1890d9
Matthias Kuhn <matthias@opengis.ch>
2017-08-26 09:52:18 +0200

    Do not pull transifex on pull requests

--- scripts/build-docs.sh
+++ scripts/build-docs.sh
@@ -6,7 +6,10 @@ set -e
 DIR=$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )/..
 pushd ${DIR}
 make clean
-make transifex_pull
+if test "$TRAVIS_SECURE_ENV_VARS" = "true" -a "$TRAVIS_BRANCH" = "master" -a "$TRAVIS_PULL_REQUEST" = "false";
+then
+    make transifex_pull
+fi
 make html
 # make all-pdf # latex doesn't like utf-8
 popd
